ad6380b0a1621832e5ef107073e938870df3e383
MarangoniDEV <livestream_jao@hotmail.com>
2021-03-03 15:09:37 -0300

    first commit

---
+++ armazenamentodados.js
@@ -0,0 +1,10 @@
+//Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa.
+
+const dados = {
+    nome: "Rocketseat",
+    cor: "roxo",
+    foco: "Programação",
+    endereco: "Rua Guilherme Gembala, 260"
+}
+
+  console.log(`A empresa ${dados.nome} está localizada em ${dados.endereco}`)
